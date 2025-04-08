28ec2ddb6ba02e4b8fb05183af9ba46a67279d23
Daniele Rangel <danielecrangel@outlook.com>
2025-04-07 23:24:28 -0300

    criada as models e services e pagina de cadastro e login, instalada a biblioteca axios

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -11,12 +11,17 @@ function Footer() {
                     <p className='text-xl font-bold'>
                             Blog Pessoal Generation | Copyright: {data}
                         </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
-                    <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
-                    </div>
+                        <div className='flex gap-2'>
+	                            <a href="https://www.linkedin.com/in/daniele-c-rangel/" target="_blank">
+    	                        <LinkedinLogo size={48} weight='bold' />
+                            </a>
+                            <a href="https://www.instagram.com/seu_usuario" target="_blank">
+    	                        <InstagramLogo size={48} weight='bold' />
+                            </a>
+                            <a href="https://www.facebook.com/seu_usuario" target="_blank">
+    	                        <FacebookLogo size={48} weight='bold' />
+	                            </a>
+                        </div>
                 </div>
             </div>
         </>
